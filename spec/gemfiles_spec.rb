cc55de3fefab70a54c56bc1146a96f4d76581454
ronen barzel <ronen@barzel.org>
2014-12-10 11:44:39 +0000

    tidy spec

--- spec/gemfiles_spec.rb
+++ spec/gemfiles_spec.rb
@@ -6,22 +6,23 @@ describe SchemaDev::Gemfiles do
     config = get_config(ruby: %W[1.9.3 2.1.5], rails: %W[4.0 4.1], db: %[sqlite3 postgresql])
     Dir.mktmpdir do |dir|
       Dir.chdir(dir) do
-
         SchemaDev::Gemfiles.build(config)
-
-        src = SchemaDev::Gemfiles::TEMPLATES_ROOT + "gemfiles"
-        diff = `diff -rq #{src} gemfiles 2>&1`.split("\n")
-        
-        # expect copy not to have entry for rails not in config
-        diff.reject!{ |d| d =~ %r[Only in #{src}: rails-(.*)] and not config.rails.include? $1 }
-        
-        # expect copy not to have entry for db not in config
-        diff.reject!{ |d| d =~ %r[Only in #{src}.*: Gemfile.(.*)] and not config.db.include? $1 }
-
-        expect(diff).to be_empty
+        expect(relevant_diff(config, "gemfiles")).to be_empty
       end
     end
   end
+
+  def relevant_diff(config, dir)
+    src = SchemaDev::Gemfiles::TEMPLATES_ROOT + dir
+    diff = `diff -rq #{src} #{dir} 2>&1`.split("\n")
+
+    # expect copy not to have entry for rails not in config
+    diff.reject!{ |d| d =~ %r[Only in #{src}: rails-(.*)] and not config.rails.include? $1 }
+
+    # expect copy not to have entry for db not in config
+    diff.reject!{ |d| d =~ %r[Only in #{src}.*: Gemfile.(.*)] and not config.db.include? $1 }
+  end
+
 end
         
 
