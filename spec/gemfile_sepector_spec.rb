f22786ef474dcf4775529ea6fffdf8a504459ccd
ronen barzel <ronen@barzel.org>
2015-01-03 15:57:11 +0000

    spec for infer_db

---
+++ spec/gemfile_sepector_spec.rb
@@ -0,0 +1,10 @@
+require 'schema_dev/gemfile_selector'
+
+describe SchemaDev::GemfileSelector do
+
+  it "infers db from env" do
+    test_dbname = 'this_is_a_test'
+    ENV['BUNDLE_GEMFILE'] = SchemaDev::GemfileSelector.gemfile(rails: '4.1', db: test_dbname).to_s
+    expect(SchemaDev::GemfileSelector.infer_db).to eq test_dbname
+  end
+end
