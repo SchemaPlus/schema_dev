4b2d8a61a976c9ebe8f74062f0b79e34d062377f
ronen barzel <ronen@barzel.org>
2015-01-25 01:14:39 +0000

    deprecate Rspec#setup_db in favor of Rspec#setup

--- lib/schema_dev/rspec.rb
+++ lib/schema_dev/rspec.rb
@@ -3,8 +3,13 @@ require_relative 'rspec/db'
 module SchemaDev
   module Rspec
 
-    def self.setup_db
+    def self.setup
       Db.setup
+    end
+
+    def self.setup_db
+      ActiveSupport::Deprecation.warn "SchemaDev::Rspec.setup_db is deprecated.  Use SchemaDev::Rspec.setup"
+      self.setup
     end
 
     def self.db_configuration
